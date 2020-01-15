4b28e17d913e311a3dc26b7544f2ab91b72a3c9a
Matheus <matheus08.pimentel@gmail.com>
2020-01-15 19:31:44 -0300

    Layout almost finished and responsive

--- src/App.js
+++ src/App.js
@@ -1,21 +1,93 @@
 import React, { useState } from 'react';
 
-// Componente: Bloco isolado de HTML, CSS e JS, o qual não interfere no restante da aplicação.
-// Propriedade: Informações que um componente Pai passa para o componente Filho.
-// Estado: Informações mantidas pelo componente (Lembrar: imutabilidade).
+import './global.css';
+import './App.css';
+import './Sidebar.css';
+import './Main.css';
 
 function App() {
-  const [counter, setCounter] = useState(0);
-
-  function incrementCounter(){
-    setCounter(counter + 1);
-  }
-
   return (
-    <>
-      <h1>Contador: {counter}</h1>
-      <button onClick={incrementCounter}>Incrementar</button>
-    </>
+    <div id="app">
+      <aside>
+        <strong>Cadastrar</strong>
+    
+        <form>
+          <div class="input-block">
+            <label htmlFor="github_username">Usuário do Github</label>
+            <input name="github_username" id="github_username" required />
+          </div>
+
+          <div class="input-block">
+            <label htmlFor="techs">Tecnologias</label>
+            <input name="techs" id="techs" required />
+          </div>
+
+          <div className="input-group">
+           <div class="input-block">
+            <label htmlFor="latitude">Latitude</label>
+            <input name="latitude" id="latitude" required />
+           </div>
+
+            <div class="input-block">
+              <label htmlFor="longitude">Longitude</label>
+              <input name="longitude" id="longitude" required />
+            </div>
+          </div>
+
+          <button type="submit">Salvar</button>
+        </form>
+      </aside>
+
+      <main>
+        <ul>
+          <li className="dev-item">
+            <header>
+              <img src="https://avatars3.githubusercontent.com/u/59742601?s=460&v=4" alt="Matheus Pimentel"/>
+            <div className="user-info">
+              <strong>Matheus Pimentel</strong>
+              <span>ReactJS, React Native, Node.js</span>
+            </div>
+            </header>
+            <p>Always looking for something new in the technology world.</p>
+            <a href="https://github.com/MatheusCPimentel">Acessar perfil no Github</a>
+          </li>
+          <li className="dev-item">
+            <header>
+              <img src="https://avatars3.githubusercontent.com/u/59742601?s=460&v=4" alt="Matheus Pimentel"/>
+            <div className="user-info">
+              <strong>Matheus Pimentel</strong>
+              <span>ReactJS, React Native, Node.js</span>
+            </div>
+            </header>
+            <p>Always looking for something new in the technology world.</p>
+            <a href="https://github.com/MatheusCPimentel">Acessar perfil no Github</a>
+          </li>
+          <li className="dev-item">
+            <header>
+              <img src="https://avatars3.githubusercontent.com/u/59742601?s=460&v=4" alt="Matheus Pimentel"/>
+            <div className="user-info">
+              <strong>Matheus Pimentel</strong>
+              <span>ReactJS, React Native, Node.js</span>
+            </div>
+            </header>
+            <p>Always looking for something new in the technology world.</p>
+            <a href="https://github.com/MatheusCPimentel">Acessar perfil no Github</a>
+          </li>
+          <li className="dev-item">
+            <header>
+              <img src="https://avatars3.githubusercontent.com/u/59742601?s=460&v=4" alt="Matheus Pimentel"/>
+            <div className="user-info">
+              <strong>Matheus Pimentel</strong>
+              <span>ReactJS, React Native, Node.js</span>
+            </div>
+            </header>
+            <p>Always looking for something new in the technology world.</p>
+            <a href="https://github.com/MatheusCPimentel">Acessar perfil no Github</a>
+          </li>
+        </ul>
+      </main>
+    </div>
+
   );
 }
 
